befecc8238c71795b9fc1d48a2aada9336a7fe37
Ziczin <68561269+Ziczin@users.noreply.github.com>
2025-07-25 19:21:34 +0400

    Реструктуризация для подключения node..js и react

--- forms/forms/__init__.py
+++
@@ -1,3 +0,0 @@
-from .login import login_form_data
-from .registration import registration_form_data
-from .form_creation import form_create_form_data
--- users/__init__.py
+++
@@ -1,5 +0,0 @@
-import re
-
-from django.template import base as template_base
-
-template_base.tag_re = re.compile(template_base.tag_re.pattern, re.DOTALL)
