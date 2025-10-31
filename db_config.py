b2b9e3e72c35430183557df31097e83b80207efe
Корнеев Александр Сергеевич <KorneevAS@sood.ru>
2025-10-31 14:21:09 +0400

    temp

---
+++ db_config.py
@@ -0,0 +1,11 @@
+NJparams = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {},
+    }
+}
